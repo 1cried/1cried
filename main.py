569d51784e72f2725a965721729327da11a81f9a
1cried <156472813+1cried@users.noreply.github.com>
2024-01-13 00:02:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+meme_dict = {
+            кринж аа,
+            лол Что-то очень смешное
+            }
+word = input(Введите непонятное слово )
+if word in meme_dict.keys()
+    print('true')
+else
+    print('false')
